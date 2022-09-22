e3c795306da5ca53204afa648f1e5387420f183e
oseeloka <oseeloka@gmail.com>
2022-09-22 16:02:49 -0700

    print number

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -17,7 +17,7 @@ char *string_toupper(char *);
 char *cap_string(char *);
 char *leet(char *);
 char *rot13(char *);
-char *infinite_add(char *n1, char *n2,char *r, int size_r);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
 char *add_strings(char *n1, char *n2, char *r, int r_index);
 
 #endif /*MAIN_H*/
