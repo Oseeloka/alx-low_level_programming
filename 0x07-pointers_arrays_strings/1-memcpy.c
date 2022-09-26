931575756e733f9a86f0595938ea73727ccf8404
oseeloka <oseeloka@gmail.com>
2022-09-26 14:40:44 -0700

    memcpy

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -9,14 +9,14 @@
  *
  * Return: A pointer to the destination buffer @dest.
  */
-char *_memcpy(char *dest, char *src, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int index;
 	char *destination = dest;
 	char *source = src;
 
 	for (index = 0; index < n; index++)
-		destinatiom[index] = source[index];
+		destination[index] = source[index];
 
 	return (dest);
 }
